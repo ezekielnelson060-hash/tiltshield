241a0550e3e27fe8bc4889e8847ad80f2716df9c
ezekielnelson060-hash <ezekielnelson060@gmail.com>
2026-08-31 15:26:34 +0100

    feat: core lib, types, supabase schema, questions

---
+++ src/lib/utils.ts
@@ -0,0 +1,18 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatCurrency(n: number, currency = "USD") {
+  try {
+    return new Intl.NumberFormat(undefined, {
+      style: "currency",
+      currency,
+      maximumFractionDigits: 0,
+    }).format(n);
+  } catch {
+    return String(Math.round(n));
+  }
+}
